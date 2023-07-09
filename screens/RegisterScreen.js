ec7afc86d090bb48e0cb7c20e1f5de9724a1768f
Koushik Mondal <darkdeavil004@gmail.com>
2023-07-09 14:22:10 +0530

    State added

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,9 +1,13 @@
 import { StyleSheet, Text, View,  SafeAreaView,TextInput, Pressable, KeyboardAvoidingView } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 
 const RegisterScreen = () => {
      
+  const[fname,setFname]= useState();
+  const[mail, setMail]= useState();
+  const[number, setNumber]=useState();
+
 const navigation= useNavigation();
   return (
     <KeyboardAvoidingView>
@@ -12,9 +16,9 @@ const navigation= useNavigation();
             <Text style={{color:"#1abc9b",  textAlign:"center", fontSize:20, fontWeight:500}} >Register</Text>
             
             <View style={{justifyContent:"center", alignItems:"center", marginTop:10}}>
-                <TextInput placeholder='Enter Your Name' style={styles.TextInput} />
-                <TextInput placeholder='Enter Mail Id' style={styles.TextInput} />
-                <TextInput placeholder='Enter Mobile No.' style={styles.TextInput} />
+                <TextInput placeholder='Enter Your Name' style={styles.TextInput} value={fname}  onChangeText={(text)=>setFname(text)}/>
+                <TextInput placeholder='Enter Mail Id' style={styles.TextInput} value={mail} onChangeText={(text)=>setMail(text)} />
+                <TextInput placeholder='Enter Mobile No.' style={styles.TextInput} value={number} onChangeText={(number)=> setNumber(number)} keyboardType='numeric' />
                 <TextInput placeholder='Enter Password' style={styles.TextInput} />
                 <TextInput placeholder='Confirm Password' style={styles.TextInput} />
             </View>
