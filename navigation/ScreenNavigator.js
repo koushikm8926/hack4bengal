8216574bf3a378920dfc1841ea201085e1a1683b
Koushik Mondal <darkdeavil004@gmail.com>
2023-07-09 12:10:06 +0530

    Register Screen Added

--- navigation/ScreenNavigator.js
+++ navigation/ScreenNavigator.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
 
 
 const ScreenNavigator = () => {
@@ -12,6 +13,7 @@ const ScreenNavigator = () => {
      <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
