4d0600f247604c67ae6fd36d7da26dae7ee92374
1-gagandeep <112457883+1-gagandeep@users.noreply.github.com>
2023-07-09 22:59:07 +0530

    RequestBlood screen ui updated

--- screens/RequestBlood.js
+++ screens/RequestBlood.js
@@ -6,9 +6,12 @@ const RequestBlood = () => {
     <SafeAreaView style={{marginTop:Platform.OS === "android" ? 50 :0,}}>
       <Text style={{fontSize:20, textAlign:'center',fontWeight:'bold'}}>Blood Request Page</Text>
      
-     <View style={{alignItems:'center', justifyContent:'center', marginTop:30,}}>
-      <TextInput placeholder='Name of the patient' style={{ width:'80%',borderRadius:7,height:50,borderBottomColor:"black", borderWidth:1,}} />
-     
+     <View style={{alignItems:'center', justifyContent:'center', margin:30,}}>
+      <TextInput placeholder="Patient's Name" style={{ width:350, borderRadius:7, height:50, borderColor:"black", borderWidth:1, padding:5, margin:8}} />
+      <TextInput placeholder="Patient's Age" style={{ width:350, borderRadius:7, height:50, borderColor:"black", borderWidth:1, padding:5, margin:8}} />
+      <TextInput placeholder="Address" style={{ width:350, borderRadius:7, height:50, borderColor:"black", borderWidth:1, padding:5, margin:8}} />
+      <TextInput placeholder="Postal code" style={{ width:350, borderRadius:7, height:50, borderColor:"black", borderWidth:1, padding:5, margin:8}} />
+      <TextInput placeholder="Blood Group" style={{ width:350, borderRadius:7, height:50, borderColor:"black", borderWidth:1, padding:5, margin:8}} />
      </View>
     
     </SafeAreaView>
