117e448fae4040c0c9478e8ee1bb8ee48f2e6472
Koushik Mondal <darkdeavil004@gmail.com>
2023-07-09 16:21:54 +0530

    Firebase Added.

---
+++ config.js
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCDDs6pEl8T4G3wy1egsQnXpm1hQXST38A",
+  authDomain: "hack4bengal-b09b8.firebaseapp.com",
+  projectId: "hack4bengal-b09b8",
+  storageBucket: "hack4bengal-b09b8.appspot.com",
+  messagingSenderId: "829869398648",
+  appId: "1:829869398648:web:603a198c6e109df2f06b8d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,12 +1,26 @@
 import { StyleSheet, Text, View,  SafeAreaView,TextInput, Pressable, KeyboardAvoidingView } from 'react-native'
 import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
-
+import { addDoc, collection, doc, setDoc } from "firebase/firestore"; 
+import { db } from '../config';
 const RegisterScreen = () => {
      
   const[fname,setFname]= useState();
   const[mail, setMail]= useState();
   const[number, setNumber]=useState();
+
+  function create(){
+    addDoc(collection(db, "User Details"), {
+      fname: fname,
+      mail:mail,
+      number:number,
+    }).then(()=>{
+      console.log("data submitted")
+      navigation.navigate("Home")
+    }).catch((error)=>{
+      console.log(error)
+    });
+  }
 
 const navigation= useNavigation();
   return (
@@ -19,12 +33,12 @@ const navigation= useNavigation();
                 <TextInput placeholder='Enter Your Name' style={styles.TextInput} value={fname}  onChangeText={(text)=>setFname(text)}/>
                 <TextInput placeholder='Enter Mail Id' style={styles.TextInput} value={mail} onChangeText={(text)=>setMail(text)} />
                 <TextInput placeholder='Enter Mobile No.' style={styles.TextInput} value={number} onChangeText={(number)=> setNumber(number)} keyboardType='numeric' />
-                <TextInput placeholder='Enter Password' style={styles.TextInput} />
+                <TextInput placeholder='Enter Password' style={styles.TextInput}  />
                 <TextInput placeholder='Confirm Password' style={styles.TextInput} />
             </View>
 
             <View style={{marginTop:30, justifyContent:'center', alignItems:'center'}}>
-                    <Pressable style={{backgroundColor:'#1abc9b', width:350,height:50,borderRadius:12,alignContent:'center', justifyContent:'center'}}>
+                    <Pressable onPress={create} style={{backgroundColor:'#1abc9b', width:350,height:50,borderRadius:12,alignContent:'center', justifyContent:'center'}}>
                         <Text style={{fontSize:16,fontWeight:'bold', color:'white', alignItems:'center', textAlign:'center'}}>Register</Text>
                     </Pressable>
             </View>
